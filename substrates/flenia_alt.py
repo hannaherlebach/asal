fixed numerical stability issues for flenia optimisation

substrates/flenia_alt.py
@@ -39,7 +39,7 @@ class FlowLenia:
         self.cfg = ConfigFLenia(SX=grid_size, SY=grid_size, C=C, c0=c0, c1=c1, nb_k=k, dd=dd, dt=dt, sigma=sigma, theta_A=theta_A, border=border)
 
         self.flenia = FlowLeniaImpl(self.cfg)
-        self.step_fn = self.flenia._build_step_fn()
+        self.step_fn = self.flenia.step_fn
         self.compile_params = compile_kernel_computer(SX=grid_size, SY=grid_size, nb_k=k)
 
     def default_params(self, rng) -> Params:
@@ -54,12 +54,13 @@ class FlowLenia:
         Note that in this version, State only has one field A, doesn't have fK.
         """
         A = jnp.zeros((self.cfg.SX, self.cfg.SY, self.cfg.C))
-        # EXTRA ---
+       
+        # --- EXTRA added by hannah ---
 
+        # --- Hardcoded
         # A = A.at[44:84, 44:84, :].set(jax.random.uniform(rng, (40, 40, self.cfg.C)))
-        # ---
-        # TODO
-        # Add uniform random noise to the centre third of the grid
+        
+        # --- Add uniform random noise to the centre third of the grid
         loc_x, loc_y = (self.cfg.SX//2, self.cfg.SY//2)
         dist = (self.cfg.SX//6, self.cfg.SY//6)
         A = A.at[loc_x-dist[0]:loc_x+dist[0], loc_y-dist[1]:loc_y+dist[1], :].set(jax.random.uniform(rng, (2*dist[0], 2*dist[1], self.cfg.C)))
@@ -69,7 +70,12 @@ class FlowLenia:
         """
         Converts params to CompiledParams
         """
+        
         compiled_params = self.compile_params(params)
+        
+        # This is what had NaNs - if optimisation stops working, uncomment and check
+        # jax.debug.print("step_state fK: {x}", x=jnp.max(compiled_params.fK))
+        
         next_state = self.step_fn(state, compiled_params)
         return next_state
 

substrates/flenia_impl_alt.py
@@ -18,6 +18,7 @@ from jax.experimental import host_callback
 def sigmoid(x):
     return 0.5 * (jnp.tanh(x / 2) + 1)
 
+# The division by w seems to have been causing the NaNs, so clip it in compute_kernels()
 ker_f = lambda x, a, w, b : (b * jnp.exp( - (x[..., None] - a)**2 / w)).sum(-1)
 
 bell = lambda x, m, s: jnp.exp(-((x-m)/s)**2 / 2)
@@ -305,12 +306,19 @@ def compile_kernel_computer(SX: int, SY: int, nb_k: int)->t.Callable[[Params], C
         Returns:
             CompiledParams: compiled params which can be used in update rule
         """
+        # HANNAH ADDED - clipping w to prevent 0 division in ker_f
+        params.w = jnp.clip(params.w, 1e-10, None)
 
         Ds = [ np.linalg.norm(np.mgrid[-mid:mid, -mid:mid], axis=0) /
               ((params.R+15) * params.r[k]) for k in range(nb_k) ]  # (x,y,k)
         K = jnp.dstack([sigmoid(-(D-1)*10) * ker_f(D, params.a[k], params.w[k], params.b[k])
                         for k, D in zip(range(nb_k), Ds)])
-        nK = K / jnp.sum(K, axis=(0,1), keepdims=True)  # Normalize kernels
+        
+        # Debugging - count number of NaN values in K if optimisation stops working
+        # jax.debug.print("Number of NaNs in K: {x}", x=jnp.sum(jnp.isnan(K)))
+
+        # HANNAH ADDED - add eps to prevent 0 division
+        nK = K / (jnp.sum(K, axis=(0,1), keepdims=True) + 1e-10)  # Normalize kernels
         fK = jnp.fft.fft2(jnp.fft.fftshift(nK, axes=(0,1)), axes=(0,1))  # Get kernels fft
 
         return CompiledParams(fK=fK, m=params.m, s=params.s, h=params.h)